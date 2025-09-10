64113ce4f442271b3da15e107f6884cc7d40c494
AhmedSaei <ahmedalsaei90@gmail.com>
2025-09-10 19:21:02 +0300

    Fix App.jsx comment

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,21 @@ import { Quiz } from './pages/Quiz';
 import { Leaderboard } from './pages/Leaderboard';
 import { Store } from './pages/Store';
 import { QuizZone } from './pages/QuizZone';
-import { PlayZone } from './pages/PlayZone';  // <-- Added
+import { PlayZone } from './pages/PlayZone';
 
 export function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route element={<Layout />}>
+          <Route element={<Layout />}>{/* Wrap all routes with Layout (adds header + bottom nav) */}
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/quiz/:category" element={<Quiz />} />
             <Route path="/leaderboard" element={<Leaderboard />} />
             <Route path="/store" element={<Store />} />
             <Route path="/quiz-zone" element={<QuizZone />} />
-            <Route path="/play-zone" element={<PlayZone />} />  // -- Added
+            <Route path="/play-zone" element={<PlayZone />} />
           </Route>
         </Routes>
       </Router>
